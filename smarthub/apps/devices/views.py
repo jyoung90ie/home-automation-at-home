8fe8da04dd815891da31fcabcf4bcc04395080d1
John Young <jyoung@cloudsmith.io>
2021-08-14 16:04:34 +0100

    Improves handling of device locations
    Device Locations can no longer be deleted if any devices are attached
    to them. User must move device(s) to another location before it can be
    deleted. Previously this would result in device also being removed.
    
    Adds redirect views for breadcrumbs to avoid 404s.

--- smarthub/apps/devices/views.py
+++ smarthub/apps/devices/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages as flash_message
+from django.db.models.deletion import ProtectedError
+from django.http.response import HttpResponseRedirect
 from django.views.generic import (
     CreateView,
     UpdateView,
@@ -7,13 +10,10 @@ from django.views.generic import (
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.views.generic.base import RedirectView
 from . import models, forms, mixins
 from ..mixins import MakeRequestObjectAvailableInFormMixin, AddUserToFormMixin
 from ..views import UUIDView
-from django.apps import apps
-from django.core.paginator import Paginator
-
-DeviceModel = models.Device
 
 
 class AddDeviceLocation(LoginRequiredMixin, AddUserToFormMixin, CreateView):
@@ -33,12 +33,41 @@ class DeleteDeviceLocation(UUIDView, mixins.LimitResultsToUserMixin, DeleteView)
     model = models.DeviceLocation
     success_url = reverse_lazy("devices:locations:list")
 
+    def delete(self, request, *args, **kwargs):
+
+        try:
+            self.object = self.get_object()
+            success_url = self.get_success_url()
+            self.object.delete()
+            return HttpResponseRedirect(success_url)
+        except ProtectedError as ex:
+            error_message = "Cannot delete item because it is being used by one or more of your devices - please update this device and try again"
+            devices = ",".join(
+                [device.friendly_name for device in ex.protected_objects]
+            )
+
+            flash_message.warning(
+                request,
+                f"{error_message}  (Devices: {devices})",
+            )
+            return HttpResponseRedirect(request.path)
+
 
 class ListDeviceLocations(mixins.LimitResultsToUserMixin, ListView):
     model = models.DeviceLocation
-    paginate_by = 10
+    paginate_by = 15
     context_object_name = "device_locations"
     template_name = "devicelocation_list.html"
+    ordering = ["created_at"]
+
+
+class UpdateDeviceLocationRedirectView(RedirectView):
+    """Redirects URL to proper update path - for breadcrumb"""
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse_lazy(
+            "devices:locations:update", kwargs={"uuid": kwargs.pop("uuid")}
+        )
 
 
 class AddDevice(
@@ -47,32 +76,41 @@ class AddDevice(
     AddUserToFormMixin,
     CreateView,
 ):
-    form_class = forms.AddDeviceForm
+    form_class = forms.DeviceForm
     template_name = "devices/device_form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # used to check user has at least one device location to attach their device to
         context["locations"] = models.DeviceLocation.objects.filter(
             user=self.request.user
         )
         return context
 
 
-class UpdateDevice(UUIDView, mixins.LimitResultsToUserMixin, UpdateView):
-    model = DeviceModel
-    fields = ["friendly_name", "device_identifier", "location", "protocol"]
+class UpdateDevice(
+    UUIDView,
+    MakeRequestObjectAvailableInFormMixin,
+    mixins.LimitResultsToUserMixin,
+    UpdateView,
+):
+    form_class = forms.DeviceForm
     template_name_suffix = "_update_form"
+
+    def get_queryset(self):
+        return models.Device.objects.filter(uuid=self.kwargs["uuid"])
 
 
 class DeleteDevice(UUIDView, mixins.LimitResultsToUserMixin, DeleteView):
-    model = DeviceModel
+    model = models.Device
     success_url = reverse_lazy("devices:list")
 
 
 class ListDevices(UUIDView, mixins.LimitResultsToUserMixin, ListView):
-    model = DeviceModel
-    paginate_by = 10
+    model = models.Device
+    paginate_by = 15
     context_object_name = "devices"
+    ordering = ["created_at"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -81,7 +119,7 @@ class ListDevices(UUIDView, mixins.LimitResultsToUserMixin, ListView):
 
 
 class DetailDevice(UUIDView, mixins.LimitResultsToUserMixin, DetailView):
-    model = DeviceModel
+    model = models.Device
     context_object_name = "device"
 
     def get_context_data(self, **kwargs):
@@ -91,7 +129,7 @@ class DetailDevice(UUIDView, mixins.LimitResultsToUserMixin, DetailView):
 
 
 class LogsForDevice(UUIDView, mixins.LimitResultsToUserMixin, ListView):
-    paginate_by = 5
+    paginate_by = 15
     context_object_name = "logs"
     template_name = "devices/device_logs.html"
     ordering = ["-created_at"]
@@ -104,5 +142,6 @@ class LogsForDevice(UUIDView, mixins.LimitResultsToUserMixin, ListView):
     def get_queryset(self):
         uuid = self.kwargs.pop("uuid")
         self.device = models.Device.objects.get(uuid=uuid)
-        queryset = self.device.get_zigbee_logs()
+        print(dir(self.device))
+        queryset = self.device.get_zigbee_messages()
         return queryset
