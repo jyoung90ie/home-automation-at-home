7cc11d63ce1009d100f057ddad70839674cc5cb7
John Young <jyoung@cloudsmith.io>
2021-08-20 18:57:59 +0100

    Adds events and triggers to admin panel

--- smarthub/apps/events/admin.py
+++ smarthub/apps/events/admin.py
@@ -1,3 +1,41 @@
+"""Customises admin pages for device related models to display more useful fields"""
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class EventTriggerInline(admin.TabularInline):
+    """Attach event triggers directly to related event object"""
+
+    model = models.EventTrigger
+    extra = 0
+    fields = (
+        "device",
+        "metadata_field",
+        "metadata_trigger_value",
+        "trigger_type",
+        "is_enabled",
+    )
+    readonly_fields = ("created_at", "updated_at")
+
+
+class EventAdmin(admin.ModelAdmin):
+    """Display event model in admin"""
+
+    list_display = (
+        "user",
+        "description",
+        "is_enabled",
+        "send_notification",
+    )
+    readonly_fields = ("created_at", "updated_at")
+
+    def get_inlines(self, request, obj):
+        """Custom logic for displaying inlines"""
+
+        inlines = [EventTriggerInline]
+
+        return inlines
+
+
+admin.site.register(models.Event, EventAdmin)
