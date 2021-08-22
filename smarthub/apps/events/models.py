3a39d62d20ec07163d1b211908ba068d7a970163
John Young <jyoung@cloudsmith.io>
2021-08-22 19:20:23 +0100

    Add `is_triggered` method
    This assesses event trigger criteria and returns a bool to indicate
    whether the device value should result in an event trigger.

--- smarthub/apps/events/models.py
+++ smarthub/apps/events/models.py
@@ -1,4 +1,6 @@
 """Captures information relating to device event triggers"""
+import logging
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls.base import reverse
@@ -6,6 +8,9 @@ from django.utils.translation import gettext_lazy as _
 
 from ..devices.models import Device
 from ..models import BaseAbstractModel
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 class Event(BaseAbstractModel):
@@ -77,3 +82,48 @@ class EventTrigger(BaseAbstractModel):
     )
     is_enabled = models.BooleanField(
         verbose_name="Enable this trigger?", default=True)
+
+    def is_triggered(self, device_value) -> bool:
+        """Compare device value to trigger value using trigger_type for comparison - returns bool
+        True    -> trigger criteria has been met
+        False   -> trigger criteria has not been met"""
+        logger.info("Start EventTrigger is_triggered()")
+        trigger_type = self.trigger_type
+        trigger_value = self.metadata_trigger_value
+
+        # convert all values to lower case
+        trigger_value = (
+            trigger_value.lower() if isinstance(trigger_value, str) else trigger_value
+        )
+        device_value = (
+            device_value.lower() if isinstance(device_value, str) else device_value
+        )
+
+        if trigger_type == EventTriggerType.EQUAL:
+            return trigger_value == device_value
+        elif trigger_type == EventTriggerType.NOT_EQUAL:
+            return trigger_value != device_value
+
+        # next set of conditions require number - if not a number return false
+        try:
+            trigger_value = (
+                int(trigger_value)
+                if int(trigger_value) == float(trigger_value)
+                else float(trigger_value)
+            )
+        except ValueError:
+            return False
+
+        if trigger_type == EventTriggerType.GREATER_THAN_OR_EQUAL:
+            return device_value >= trigger_value
+        elif trigger_type == EventTriggerType.GREATER_THAN:
+            return device_value > trigger_value
+        elif trigger_type == EventTriggerType.LESS_THAN:
+            return device_value < trigger_value
+        elif trigger_type == EventTriggerType.LESS_THAN_OR_EQUAL:
+            return device_value <= trigger_value
+
+        logger.info("Finish EventTrigger is_triggered()")
+
+    def __str__(self) -> str:
+        return f"Trigger Settings [Device={self.device} - Field={self.metadata_field} - Type={self.trigger_type} - Value={self.metadata_trigger_value}]"
