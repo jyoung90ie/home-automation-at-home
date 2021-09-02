bfc5991bad106f1cc64a6a62d00f8e2746459c6d
John Young <jyoung@cloudsmith.io>
2021-09-02 09:01:53 +0100

    Adds JS toggle functionality

--- smarthub/apps/mqtt/views.py
+++ smarthub/apps/mqtt/views.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from .defines import MQTT_STATE_COMMAND, MQTT_STATE_TOGGLE_VALUE
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404, HttpResponse, JsonResponse
 from django.views.generic.base import View
 from django.shortcuts import get_object_or_404
 from ..views import UUIDView
@@ -21,22 +21,13 @@ class ToggleDeviceState(UUIDView, View):
 
     http_method_names = [
         "post",
-        "get",
     ]
-
-    def get(self, request, *args, **kwargs):
-        # TODO - remove - this is only for testing
-        return self.post(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         """Handle post request"""
 
-        # default response - overridden on success
-        response_status_code = HTTPStatus.BAD_REQUEST
-
         device_uuid = self.kwargs.pop("duuid", None)
 
-        # TODO - make sure user owns device
         device = get_object_or_404(Device, user=self.request.user, uuid=device_uuid)
 
         if device and not device.is_linked():
@@ -53,12 +44,23 @@ class ToggleDeviceState(UUIDView, View):
                 command_value=MQTT_STATE_TOGGLE_VALUE,
             )
             logger.info("%s - toggle command sent", __name__)
-            response_status_code = HTTPStatus.NO_CONTENT  # success - no content
+
+            response = {
+                "status": "success",
+                "message": f"{device.friendly_name.title()} has been toggled",
+            }
+
+            # response_status_code = HTTPStatus.NO_CONTENT  # success - no content
 
         except Exception as ex:
             logger.info("%s - there was a problem sending toggle command", __name__)
+            response = {
+                "status": "error",
+                "message": f"{device.friendly_name.title()} could not be toggled - check the device settings and try again",
+            }
 
-        return HttpResponse(status=response_status_code)
+        return JsonResponse(response)
+        # return HttpResponse(status=response_status_code)
 
 
 class TriggerDeviceState(UUIDView, View):
--- smarthub/static/js/main.js
+++ smarthub/static/js/main.js
@@ -1,29 +1,64 @@
-// var deviceList = document.getElementById('device-list')
-// if (deviceList) {
-//     var tooltip = new bootstrap.Tooltip(deviceList, { animation: true })
-// }
+const createAlertElement = (alertMessage, alertType) => {
+    if (!alertMessage) return;
 
+    if (!alertType) {
+        alertType = "alert-success"
+    }
+    const mainBodyContainer = document.querySelector("body > div.container");
 
-// device comms
+    const alertContainerHTML = '<div class="alert ' + alertType + ' alert-dismissible fade show" role="alert">' +
+        alertMessage +
+        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' +
+        '</div>';
 
+    alertElement = $(alertContainerHTML);
+
+    // remove any existing alerts before creating new one
+    if ($("." + alertType).length > 0) {
+        $("." + alertType).remove();
+    }
+
+    $(mainBodyContainer).prepend(alertElement);
+
+    clearTimeout();
+    setTimeout(() => $(alertElement).remove(), 5000);
+}
 
 const deviceStateClass = ".change-device-state"
 
 
 if ($(deviceStateClass).length > 0) {
+
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
     $(deviceStateClass).on("click", (event) => {
         // event.preventDefault();
-        url = event.target.baseURI
-        console.log("url", url)
-        // var url = event.target.parent().action
-        // console.log("url", url)
+        url = event.currentTarget.action
+
+        const request = new Request(
+            url,
+            { headers: { "X-CSRFToken": csrftoken } }
+
+        );
+
+        fetch(request, {
+            method: "POST",
+            mode: "same-origin"
+        })
+            .then(response => response.json())
+            .then(response => {
+
+                if (response.status == "error") {
+                    status = "alert-danger"
+                } else {
+                    status = "alert-success"
+                }
+
+                createAlertElement(response.message, status);
+            })
+
         return false;
     })
 }
 
 
-// const makeRequest = new Request(
-
-// )
